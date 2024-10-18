c5c2307dc8e2591ba1e6f0a70cfcd3de745f3c79
khairin <khairin13chan@gmail.com>
2024-10-18 16:54:34 +0800

    :art: add dashboard bg

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Dashboard = () => {
   return (
     <div className="dark:bg-secondaryBlack inset-0 flex min-h-screen w-full flex-col items-center justify-center bg-white bg-[linear-gradient(to_right,#80808033_1px,transparent_1px),linear-gradient(to_bottom,#80808033_1px,transparent_1px)] bg-[size:70px_70px]">
+      welcome to tadarus.my
     </div>
   )
 }
