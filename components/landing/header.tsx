bdd32e8d447d857b87d27e7bb5688fcea6b1b902
khairin <khairin13chan@gmail.com>
2024-10-18 14:58:47 +0800

    :art: add auth and onboarding form

--- components/landing/header.tsx
+++ components/landing/header.tsx
@@ -1,7 +1,11 @@
+'use client'
 
 import { Button } from '@/components/ui/button'
+import { useRouter } from 'next/navigation';
 
 export default function Header() {
+    const router = useRouter();
+
     return (
         <header className="dark:bg-secondaryBlack inset-0 flex min-h-screen w-full flex-col items-center justify-center bg-white bg-[linear-gradient(to_right,#80808033_1px,transparent_1px),linear-gradient(to_bottom,#80808033_1px,transparent_1px)] bg-[size:70px_70px]">
             <div className="mx-auto w-container max-w-full px-5 py-[110px] text-center lg:py-[150px]">
@@ -14,6 +18,7 @@ export default function Header() {
                 <Button
                     size="lg"
                     className="h-12 text-base font-heading md:text-lg lg:h-14 lg:text-xl"
+                    onClick={() => router.push('/dashboard')}
                 >
                     Get started
                 </Button>
