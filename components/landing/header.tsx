724b23b5239b0a3d74075c071833a43d810d4a43
khairin <khairin13chan@gmail.com>
2024-10-18 16:32:18 +0800

    :art: add animation for landing page header

--- components/landing/header.tsx
+++ components/landing/header.tsx
@@ -2,15 +2,22 @@
 
 import { Button } from '@/components/ui/button'
 import { useRouter } from 'next/navigation';
+import { TypewriterEffectSmooth } from '../ui/typewriter-effect';
 
 export default function Header() {
     const router = useRouter();
+
+    const words = [
+        {
+            text: 'tadarus.my',
+        },
+    ]
 
     return (
         <header className="dark:bg-secondaryBlack inset-0 flex min-h-screen w-full flex-col items-center justify-center bg-white bg-[linear-gradient(to_right,#80808033_1px,transparent_1px),linear-gradient(to_bottom,#80808033_1px,transparent_1px)] bg-[size:70px_70px]">
             <div className="mx-auto w-container max-w-full px-5 py-[110px] text-center lg:py-[150px]">
                 <h1 className="bricolage text-3xl font-heading md:text-4xl lg:text-5xl">
-                    tadarus.my
+                    <TypewriterEffectSmooth words={words} />
                 </h1>
                 <p className="my-12 mt-8 text-lg font-normal leading-relaxed md:text-xl lg:text-2xl lg:leading-relaxed">
                     your ramadhan companion.
